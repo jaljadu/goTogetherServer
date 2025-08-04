8aaa63d7c2a85a7772c3248ec520dfa9750c8bff
jaljadu <jaljadu@gmail.com>
2025-08-04 06:35:43 +0530

    ww

--- src/locationsearch.controller.ts
+++ src/locationsearch.controller.ts
@@ -4,7 +4,9 @@ import { env } from 'process';
 export const getLocation = async (req: Request, res: Response)=> {  
   console.log(req); 
   const input = req.query.input; 
-  const response = await fetch(`${process.env.GOOGLE_MAP_URL}place/autocomplete/json?input=${input}&key=${process.env.GOOGLE_API_KEY}&language=en`);
+  var url=`${process.env.GOOGLE_MAP_URL}place/autocomplete/json?input=${input}&key=${process.env.GOOGLE_API_KEY}&language=en`;
+  console.log(url);
+  const response = await fetch(url);
   const data = await response.json();
   console.log(data);
   res.json(data);
@@ -13,7 +15,7 @@ export const getDirectionByPlace = async (req: Request, res: Response)=> {
   console.log(req); 
   const sourcePlaceId = req.query.sourcePlaceId; 
   const destinationPlaceId = req.query.destinationPlaceId; 
-  const response = await fetch(`${process.env.GOOGLE_MAP_URL}directions/json?origin=${sourcePlaceId}&destination=${destinationPlaceId}&key=${process.env.GOOGLE_API_KEY}`);
+  const response = await fetch(`${process.env.GOOGLE_MAP_URL}directions/json?origin=place_id:${sourcePlaceId}&destination=place_id:${destinationPlaceId}&key=${process.env.GOOGLE_API_KEY}`);
   const data = await response.json();
   console.log(data);
   res.json(data);
@@ -22,7 +24,8 @@ export const reverseGeocode = async (req: Request, res: Response)=> {
   console.log(req); 
   const lat = req.query.lat; 
   const lng = req.query.lng; 
-  const response = await fetch(`${process.env.GOOGLE_MAP_URL}geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_API_KEY}`);
+  var url=`${process.env.GOOGLE_MAP_URL}geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_API_KEY}`
+  const response = await fetch(url);
   const data = await response.json();
   console.log(data);
   res.json(data);
